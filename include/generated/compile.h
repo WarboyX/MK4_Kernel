c6a8c120f639ffd64b42c3162593cb1ad8da2267
warbuild-vmware <warbuild@ubuntu.(none)>
2013-12-12 05:01:42 -0800

    MK4v1

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Mon Nov 4 14:42:28 KST 2013"
+#define LINUX_COMPILE_BY "usagsm"
+#define LINUX_COMPILE_HOST "shinalee-desktop"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
